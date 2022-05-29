5897b30166111770f71dcf158e70c8e27b0620a3
Vishal Srivastava <eth.vishal@gmail.com>
2022-05-29 09:55:18 +0530

    Modified index.js file and created a file contractData/index.js which contains contract abi and address to interact with. And now the app is working.

--- frontend/contractData/index.js
+++ frontend/contractData/index.js
@@ -1,1 +1,53 @@
-export 
+export const contractABI = [
+    {
+      "inputs": [
+        {
+          "internalType": "uint8",
+          "name": "_maxWhitelistedAddress",
+          "type": "uint8"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "constructor"
+    },
+    {
+      "inputs": [],
+      "name": "addToWhitelist",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "maxWhitelistedAddresses",
+      "outputs": [
+        {
+          "internalType": "uint8",
+          "name": "",
+          "type": "uint8"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "name": "whitelistedAddresses",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    }
+  ];
+export const contractAddress = "0x549bc9E2e092a6627a014E8B207887E39fb25D8d";
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,9 +1,11 @@
 import Head from 'next/head'
-import Image from 'next/image'
+//import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Web3Modal from 'web3modal'
 import {providers,Contract} from 'ethers'
 import {useEffect, useRef, useState} from 'react'
+import {contractAddress,contractABI} from '../contractData/index.js'
+
 
 
 
@@ -11,13 +13,66 @@ export default function Home() {
   
   const [walletConnectionStatus, setWalletConnectionStatus] = useState(false);
   const [loading, setLoading] = useState(true);
-  const [subsbribeWhitelist,setSubsbcribeWhitelist] = useState(false);
+  const [subsbribedWhitelist,setSubsbcribedWhitelist] = useState(false);
   const web3ModalRef = useRef();
   
+
+  const getProviderOrSigner = async (needSigner = false) =>{
+    const provider = await web3ModalRef.current.connect();
+    const web3Provider = new providers.Web3Provider(provider);
+    const {chainId} = await web3Provider.getNetwork();
+    if(chainId!==4){
+      window.alert("Please change your network to rinkeby testnet.");
+      throw new Error("Please change your network to rinkeby testnet.");
+    }
+    if(needSigner){
+      const signer = await web3Provider.getSigner();
+      return signer;
+    }
+    return web3Provider;
+  }
+  
+  const checkIfWhitelisted = async () =>{
+    try{
+      const signer = await getProviderOrSigner(true);
+      const whitelistContract = new Contract(
+        contractAddress,
+        contractABI,
+        signer
+      );
+      const address = await signer.getAddress();
+      const isWhitelisted = await whitelistContract.whitelistedAddresses(address);
+      setSubsbcribedWhitelist(isWhitelisted);
+    }catch(err){
+      console.log(err);
+    }
+  }
+  
+  const addAddressToWhitelist = async()=>{
+    try{
+      const signer = await getProviderOrSigner(true);
+      const whitelistContract = new Contract(
+        contractAddress,
+        contractABI,
+        signer
+      );
+      const tx = await whitelistContract.addToWhitelist();
+      setLoading(true);
+      await tx.wait();
+      setLoading(false);
+      setSubsbcribedWhitelist(true);
+
+    }catch(err){
+      console.log(err);
+    }
+  }
   
   const connectWallet = async () => {
     await getProviderOrSigner();
     setWalletConnectionStatus(true); 
+    await checkIfWhitelisted();
+    setLoading(false);
+
   }
   
   
@@ -32,5 +87,57 @@ export default function Home() {
     connectWallet();
   },[walletConnectionStatus]); 
   
+  const renderButton = () =>{
+    if(walletConnectionStatus){
+      if(subsbribedWhitelist){
+        return(
+          <div className={styles.description}>
+            Thanks for joining the Whitelist!
+          </div>
+        );
+      } else if(loading){
+        return <button className={styles.button}>Loading...</button>;
+      } else {
+        console.log("Button rendering");
+        return <button onClick={addAddressToWhitelist} className={styles.button}>
+            Join the Whitelist
+          </button>
+      }
+    } else {
+      return <button onClick={connectWallet} className={styles.button}>
+            Connect Wallet
+          </button>
+    }
+
+  }
+  
+  
+  return( <div>
+    <Head>
+      <title>Whitelist Dapp</title>
+      <meta name="description" content="Whitelist-Dapp" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <div className={styles.main}>
+      <div>
+        <h1 className={styles.title}>Welcome to Crypto Devs!</h1>
+        <div className={styles.description}>
+          Its an NFT collection for developers in Crypto.
+        </div>
+        {/* <div className={styles.description}>
+          {numberOfWhitelisted} have already joined the Whitelist
+        </div> */}
+        {renderButton()}
+      </div>
+      <div>
+        <img className={styles.image} src="./crypto-devs.svg" />
+      </div>
+    </div>
+
+    <footer className={styles.footer}>
+      Made with &#10084; by Crypto Devs
+    </footer>
+  </div>
+);
  
 }
